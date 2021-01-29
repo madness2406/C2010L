62e346aefee02b5a104d1b96df8dc732593b8cbf
madness2406 <78027722+madness2406@users.noreply.github.com>
2021-01-29 20:53:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<meta charset="utf-8">
+</head>
+<body style="background-color: grey">
+	<a href="Bai1.html">Bai 1</a><br>
+	<a href="GalleryOne.html">Bai 2</a><br>
+</body>
+</html>
